e7fac9c74e1e6783e0d97ca69484c875ce3b8f5a
Florian Pesth <fpesth@gmx.de>
2023-10-17 07:03:08 +0200

    Improve robustness by assuming single block of text

--- scantotask.sh
+++ scantotask.sh
@@ -20,7 +20,9 @@ tesseract \
   $temp_png_file \
   $temp_txt_file_without_suffix \
   --dpi 600 \
-  -l deu
+  -l deu \
+  --psm 4
+cat $temp_txt_file
 awk '/^[0-9A]+[ ]*./{if(length(currenttask)!=0) {print currenttask; } currenttask=$0; sub(/^[0-9A]+[ ]*.[ ]*/,"",currenttask);}; !/^[0-9A]+[ ]*./{if(length(currenttask)!=0) {currenttask=currenttask" "$0;}}' $temp_txt_file | while read -r task
 do
   task add $task
